b1b9f17d39ac243ff347d4ac40f0f414f76a8367
unknown <rodion.ermakov@gmail.com>
2019-05-12 12:32:13 +0300

    сделал слайдер, 2 страницы, добавил стили

---
+++ views/main/company.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="/css/slider.css">
+  <link rel="stylesheet" href="/css/header.css">
+  <title></title>
+</head>
+<body>
+<div class="header" id="head-menu">
+    <div class="logo">
+		<div class="logo-item">
+			<img src="/img/logo.png">
+		</div>
+		<div class="log-in">
+			<span><a href="#">Авторизация</a></span>
+			<span><a href="#">Регистрация</a></span>
+		</div>
+    </div>
+	
+	<div class="top-menu">
+		<div class="menu">
+			<ul>
+				<li><a href="#">Стажировки</a></li>
+				<li><a href="#">Компании</a></li>
+				<li><a href="#">Первая работа</a></li>
+			</ul>
+		</div>
+		<div class="fj">
+			<a href="#">Разместить стажировку</a>
+		</div>
+	</div>
+	
+</div>
+
+<div id="slider" class="slider">
+    <ol class="slider__indicators">
+      <li class="slider__indicator slider__indicator_active" data-slide-to="0"></li>
+      <li class="slider__indicator" data-slide-to="1"></li>
+      <li class="slider__indicator" data-slide-to="2"></li>
+      <li class="slider__indicator" data-slide-to="3"></li>
+    </ol>
+    <div class="slider__items">
+      <div class="slider__item slider__item_1 slider__item_active">
+        <div class="slider__item_inner">
+          <span class="slider__item_title">
+            Lorem ipsum dolor
+          </span>
+          <span class="slider__items_description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nihil quo molestiae perspiciatis qui vero?
+          </span>
+        </div>
+      </div>
+      <div class="slider__item slider__item_2">
+        <div class="slider__item_inner">
+          <span class="slider__item_title">
+            Lorem ipsum dolor
+          </span>
+          <span class="slider__items_description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nihil quo molestiae perspiciatis qui vero?
+          </span>
+        </div>
+      </div>
+      <div class="slider__item slider__item_3">
+        <div class="slider__item_inner">
+          <span class="slider__item_title">
+            Lorem ipsum dolor
+          </span>
+          <span class="slider__items_description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nihil quo molestiae perspiciatis qui vero?
+          </span>
+        </div>
+      </div>
+      <div class="slider__item slider__item_4">
+        <div class="slider__item_inner">
+          <span class="slider__item_title">
+            Lorem ipsum dolor
+          </span>
+          <span class="slider__items_description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nihil quo molestiae perspiciatis qui vero?
+          </span>
+        </div>
+      </div>
+    </div>
+    <a class="slider__control slider__control_prev" href="#" role="button"></a>
+    <a class="slider__control slider__control_next" href="#" role="button"></a>
+</div>
+<div class = "container">
+    <h1 class="sj_typo_h1 Index_h1">Компании</h1>
+	<div class="CompanyList_item sj_stu_block h_clearfix">
+		<div class="CompanyList_item_left">
+			<img src="/img/matter.png"class="StudentCompanyListItem_logo">
+		</div>		
+		<div class="CompanyList_item_right">
+			<a href="" class="h_color_black h_border_none">
+				<h2 class="sj_typo_h2 CompanyList_item_company h_color_black">Solo-it Studio, Самара</h2>
+			</a>
+			<div class="CompanyList_item_catalog h_color_gray_lt"></div>
+			<div class="CompanyList_item_about h_color_black">
+				Разработка, продвижение и поддержка сайтов.
+			</div>
+			<div class="CompanyList_item_contents">		
+				<span class="CompanyList_item_contents_item_text">330 стажировок</span>
+			</div>          
+		</div>  
+	</div>
+	<div class="matter_link">
+        <a href="#" class="sj_btn">Все компании</a>
+    </div>
+</div>
+<div class="sj_l_footer">
+    <footer class="FooterWr m_unregistered">
+        <div class="sj_l_content">
+            <div class="FooterNav">
+                <ul class="FooterNav_list m_whom_menu">
+                    <li class="FooterNav_list_item">
+                        <a href="#" target="_blank"><span>Студенту</span></a>
+                    </li>
+                    <li class="FooterNav_list_item">
+                        <a href="#" target="_blank"><span>Работодателю</span></a>
+                    </li>
+                </ul>
+                <ul class="FooterNav_list m_main_menu">
+                    <li class="FooterNav_list_item">
+                        <a href="#"><span>Стажировки</span></a>
+                    </li>
+                    <li class="FooterNav_list_item">
+                        <a href="#"><span>Компании</span></a>
+                    </li>
+                    <li class="FooterNav_list_item">
+                        <a href="#" target="_blank"><span>Правила работы</span></a>
+                    </li>
+                </ul>
+                <ul class="FooterNav_list m_aux_menu">
+                    <li class="FooterNav_list_item">
+                        <a href="#" target="_blank"><i class="fa fa-envelope"></i>&nbsp;<span>Обратная связь</span></a>
+                    </li>
+                </ul>
+            </div>
+			<hr class="FooterSeparator FooterSeo">
+            <p class="FooterSeoSlogan FooterSeo">
+                Мечтаете о&nbsp;головокружительной карьере? Начинать никогда не&nbsp;рано! Стажировка для студентов или первая работа&nbsp;&mdash; отличный старт. И&nbsp;хотя скептики говорят, что без опыта нет работы, а&nbsp;без работы нет опыта, мы&nbsp;знаем, как разорвать этот порочный круг. Приходите на&nbsp;FirstJob.ru, заходите в&nbsp;раздел &laquo;Работа для студентов&raquo;&nbsp;&mdash; здесь вы&nbsp;обязательно найдете подходящую вам стажировку, место для преддипломной практики на&nbsp;предприятии или работу для студентов со&nbsp;свободным графиком.
+                &laquo;Работа для студентов&raquo;&nbsp;&mdash; уникальный проект портала FirstJob.ru. Это онлайн-площадка для общения работодателей с&nbsp;будущими выпускниками вузов. Здесь компании и&nbsp;заинтересованные студенты находят друг друга, даже несмотря на&nbsp;разделяющее их&nbsp;расстояние.
+                FirstJob.ru гарантирует: чем раньше вы&nbsp;начнете пользоваться нашим сервисом, тем успешнее сложится ваша карьера. Вы&nbsp;научитесь видеть свои профессиональные возможности, планировать карьеру, а&nbsp;по&nbsp;окончании вуза найдете лучшую для себя работу по&nbsp;специальности.
+                Не&nbsp;упускайте свой шанс&nbsp;&mdash; подыскивайте себе интересную стажировку, практику или работу для студентов с&nbsp;гибким графиком. Не&nbsp;ждите получения диплома&nbsp;&mdash; начинайте прямо сейчаc!
+            </p>
+            <div class="FooterLogo">
+                <img src="/img/logo.png">
+            </div>
+        </div>
+    </footer>
+</div>   
+<script src="/js/slider.js"></script>
+</body>
+</html>							
