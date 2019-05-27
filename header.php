abdf02f4f085f46bc921c5fb3d17b23a05c30186
Rodion <rodion.ermakov@gmail.com>
2019-05-27 09:43:57 +0300

    проверка подключения

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-  <meta charset="utf-8"> <!-- -->
+  <meta charset="utf-8">
+  <meta http-equiv="Refresh" content="30"><!-- перезагрузка страницы  -->
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="/css/slider.css">
   <link rel="stylesheet" href="/css/header.css">
