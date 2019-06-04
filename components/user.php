fb0c55431e0830064e8fdce80f1c8e63687722a4
Rodion <rodion.ermakov@gmail.com>
2019-06-04 16:11:42 +0300

    делаю верстку детальной стажировок

--- components/user.php
+++ components/user.php
@@ -4,8 +4,6 @@ session_start();
 
 $users = $_SESSION['login'];
 
-
-
 $check_query = $mysqli->query("SELECT b.name, b.abode FROM users a, student b WHERE a.id = b.id_user AND a.id = ".$users['id']." ");
 while($check_result = $check_query->fetch_assoc()){
     $user = $check_result;
---
+++ views/main/detail_job.php
@@ -0,0 +1,44 @@
+<?require(ROOT. '/views/main/header.php');?>
+<div class="detail">
+    <div class="left_block">
+        <div class="name_internship">
+            Контент-менеджер
+        </div>
+        <div class="tasks">
+            <h3>Какие задачи выпонять?</h3>
+            Наполнение сайта информацией: данные гидов, описание экскурсий (русский и англ. язык)
+            Общение с гидами по телефону
+            Подбор фотографий для сайта
+        </div>
+        <div class="conditions">
+            <h3>Условия стажировки</h3>
+            Офис 7 минут от ст.м. Выборгская
+            Стажировка с 11.00 до 16.00
+            Компенсация питания 250 руб. ежедневно
+            Возможно трудоустройство по факту успешного прохождения стажировки
+        </div>
+        <div class="skills">
+            <h3>Какие навыки требуются?</h3>
+            Уверенный пользователь ПК
+            Английский язык на уровне не ниже Intermidiate
+            Коммуникативные навыки
+        </div>
+        <div class="respond">Откликнутся на стажировку</div>
+    </div>
+    <div class="right_block">
+        <div class="company_logo">
+            <img src="/img/matter.png">
+        </div>
+        <div class="company_name">Турист Клаб</div>
+        <div class="industry">
+            <h5>Отрасль</h5>
+            Разработка, программирование
+        </div>
+        <div class="date">
+            Срок проведения
+            10 июн 2019 — 31 июл 2019
+        </div>
+    </div>
+</div>
+
+<?require(ROOT. '/views/main/footer.php');?>
